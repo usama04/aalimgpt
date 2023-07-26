596fc288fb7561f85388ea469ecc9d3920f78de4
usama04 <16793424+usama04@users.noreply.github.com>
2023-07-26 08:47:44 +0000

    Reverted all changes

--- backend/api/api.py
+++ backend/api/api.py
@@ -17,8 +17,6 @@ from fastapi.responses import HTMLResponse
 openai.api_key = settings.OPENAI_API_KEY
 
 app = FastAPI(
-    docs_url="/api/docs",
-    redoc_url="/api/redoc",
     default_response_class=CustomJSONResponse,
 )
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
